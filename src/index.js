d32e35d6d40bccca67d8bb258d1334f2b9485e3f
EC2 Default User <ec2-user@ip-10-0-0-217.ec2.internal>
2021-04-20 16:31:05 +0000

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import Amplify from 'aws-amplify'
+import config from './aws-exports'
+Amplify.configure(config)
+
 
 ReactDOM.render(
   <React.StrictMode>
